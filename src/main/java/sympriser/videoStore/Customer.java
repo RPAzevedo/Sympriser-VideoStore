7639c80a8cbcce747d5469683f1220241b292d48
Rafael Peixoto de Azevedo <rafael.peixoto.de.azevedo@gmail.com>
2010-05-22 18:31:39 -0300

    Delegating rental charge calculation to the Rental class.

--- src/main/java/sympriser/videoStore/Customer.java
+++ src/main/java/sympriser/videoStore/Customer.java
@@ -4,70 +4,51 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Customer {
-    
-    private String _name;
+
+    private String       _name;
     private List<Rental> _rentals = new ArrayList<Rental>();
 
-    public Customer (String name) {
+    public Customer(String name) {
+
         _name = name;
     }
 
     public void addRental(Rental arg) {
+
         _rentals.add(arg);
     }
 
     public String getName() {
+
         return _name;
     }
 
     public String statement() {
 
-        double      totalAmount  = 0;
-        int         renterPoints = 0;
-        String      result       = "Rental Record for " + getName() + "\n";
+        double totalCharge = 0.00;
+        int    renterPoints = 0;
+        
+        String result = "Rental Record for " + getName() + "\n";
 
-        for (Rental each : _rentals) {
-
-            double thisAmount = 0;
-
-            // determine amounts for each rental
-            switch (each.getVideo().getPriceCode()) {
-                case Video.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2) {
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    }
-                    break;
-                case Video.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Video.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3) {
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    }
-                    break;
-            }
-
+        for (Rental each : _rentals)
+        {
+            double thisCharge = each.getCharge();
+            
             // add frequent renter points
             renterPoints++;
 
             // add bonus for a two day new release rental
-            if ((each.getVideo().getPriceCode() == Video.NEW_RELEASE) &&
-                (each.getDaysRented() > 1)) {
-                    renterPoints++;
-            }
+            if ((each.getVideo().getPriceCode() == Video.NEW_RELEASE) && (each.getDaysRented() > 1))
+                renterPoints++;
 
             // show figures for this rental
-            result += "\t" + each.getVideo().getTitle() +
-                      "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getVideo().getTitle() + "\t" + String.valueOf(thisCharge) + "\n";
+            totalCharge += thisCharge;
         }
 
         // add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(renterPoints) +
-                  " frequent renter points";
+        result += "Amount owed is " + String.valueOf(totalCharge) + "\n";
+        result += "You earned " + String.valueOf(renterPoints) + " frequent renter points";
 
         return result;
     }
--- src/main/java/sympriser/videoStore/Rental.java
+++ src/main/java/sympriser/videoStore/Rental.java
@@ -1,20 +1,48 @@
 package sympriser.videoStore;
 
 public class Rental {
-    
+
     private Video _video;
     private int   _daysRented;
 
     public Rental(Video movie, int daysRented) {
-        _video      = movie;
+
+        _video = movie;
         _daysRented = daysRented;
     }
 
     public int getDaysRented() {
+
         return _daysRented;
     }
 
     public Video getVideo() {
+
         return _video;
+    }
+
+    public double getCharge() {
+
+        double charge;
+
+        switch (this.getVideo().getPriceCode()) {
+        case Video.REGULAR:
+            charge = 2;
+            if (this.getDaysRented() > 2) 
+                charge += (this.getDaysRented() - 2) * 1.5;
+            break;
+        case Video.NEW_RELEASE:
+            charge = this.getDaysRented() * 3;
+            break;
+        case Video.CHILDRENS:
+            charge = 1.5;
+            if (this.getDaysRented() > 3) 
+                charge += (this.getDaysRented() - 3) * 1.5;
+            break;
+        default:
+            charge = 0.00;
+        }
+
+        return charge;
     }
 }
